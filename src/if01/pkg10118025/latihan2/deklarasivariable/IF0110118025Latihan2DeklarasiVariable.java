9ccd07ff3ebf33519528170ad230b4d1730ba27f
rizki2704 <55817452+rizki2704@users.noreply.github.com>
2019-09-26 08:01:29 +0700

    Add files via upload

---
+++ src/if01/pkg10118025/latihan2/deklarasivariable/IF0110118025Latihan2DeklarasiVariable.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if01.pkg10118025.latihan2.deklarasivariable;
+
+/**
+ *
+ * @author
+ * Nama     : Rizki Restu Illahi
+ * Kelas    : IF01
+ * NIM      : 10118025
+ */
+public class IF0110118025Latihan2DeklarasiVariable {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int nilaiInt;
+        final double PHI = 3.14; //konstanta uppercase
+        boolean nilaiLogika;
+        char nilaiKarakter;
+        
+        //memberi nilai ke variable
+        nilaiInt = 78;
+        nilaiLogika = false;
+        nilaiKarakter = 'D';
+        
+        //menampilkan hasil
+        System.out.println();
+        System.out.println("Isi Nilai Variable nilai = " + nilaiInt);
+        System.out.println("Isi Variable PHI = " + PHI);
+        System.out.println("Isi Variable Logika = " + nilaiLogika);
+        System.out.println("Isi Variable Karakter = " + nilaiKarakter);
+    }
+    
+}
